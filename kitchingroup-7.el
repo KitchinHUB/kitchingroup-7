304d4643d1748bed5e9669a0e9079d8620dea971
John Kitchin <jkitchin@andrew.cmu.edu>
2016-01-31 18:44:37 -0500

    update headers

--- kitchingroup-7.el
+++ kitchingroup-7.el
@@ -1,11 +1,43 @@
-;;; kitchingroup-7.el --- CaPPA preprint for doi:
+;;; kitchingroup-7.el --- CaPPA preprint for doi:10.1063/1.1737365
 
 ;; Copyright (C) 2016 John Kitchin
 
 ;; Version: 0.0.1
 ;; Author: John Kitchin <jkitchin@andrew.cmu.edu>
+;; Journal: Journal of Chemical Physics
+;; DOI: 10.1063/1.1737365
+;; Bibtex: @article{kitchin-2004-modif-pt,
+;;   author =	 {Kitchin, J. R. and N{\o}rskov, J. K. and Barteau, M. A. and
+;;                   Chen, J.  G.},
+;;   title =	 {Modification of the Surface Electronic and Chemical Properties
+;;                   of {Pt}(111) By Subsurface 3d Transition Metals},
+;;   journal =	 {Journal of Chemical Physics},
+;;   volume =	 120,
+;;   number =	 21,
+;;   pages =	 {10240-10246},
+;;   year =	 2004,
+;;   doi =	 {10.1063/1.1737365},
+;;   abstract =	 {The modification of the electronic and chemical properties of
+;;                   Pt(111) surfaces by subsurface 3d transition metals was
+;;                   studied using density-functional theory. In each case
+;;                   investigated, the Pt surface d-band was broadened and lowered
+;;                   in energy by interactions with the subsurface 3d metals,
+;;                   resulting in weaker dissociative adsorption energies of
+;;                   hydrogen and oxygen on these surfaces. The magnitude of the
+;;                   decrease in adsorption energy was largest for the early 3d
+;;                   transition metals and smallest for the late 3d transition
+;;                   metals. In some cases, dissociative adsorption was calculated
+;;                   to be endothermic. The surfaces investigated in this study had
+;;                   no lateral strain in them, demonstrating that strain is not a
+;;                   necessary factor in the modification of bimetallic surface
+;;                   properties. The implications of these findings are discussed
+;;                   in the context of c talyst design, particularly for fuel cell
+;;                   electrocatalysts.  (C) 2004 American Institute of Physics.},
+;;   issn =	 {0021-9606},
+;;   type =	 {Journal Article},
+;;  }
 
-;; TBD
+
 
 ;;; Commentary:
 
@@ -16,23 +48,6 @@
 
 
 (provide 'kitchingroup-7)
-;;; kitchingroup-7.el ends here
-;;; kitchingroup-7.el --- CaPPA preprint for doi:
-
-;; Copyright (C) 2016 John Kitchin
-
-;; Version: 0.0.1
-;; Author: John Kitchin <jkitchin@andrew.cmu.edu>
-
-;; TBD
-
-;;; Commentary:
-
-;;; Code:
-(require 'cappa)
-
-(cappa-register 'kitchingroup-7)
-
 
 (provide 'kitchingroup-7)
 ;;; kitchingroup-7.el ends here
